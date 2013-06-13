274e3e7c6b86e5e62efae95fbafa43ea45d2d006
Graham Ollis <perl@wdlabs.com>
2013-06-13 14:40:43 -0400

    app version of plugauthlite

--- lib/PlugAuth/Lite.pm
+++ lib/PlugAuth/Lite.pm
@@ -3,9 +3,20 @@ package PlugAuth::Lite;
 use strict;
 use warnings;
 use v5.10;
+use Mojo::Base qw( Mojolicious );
 
 # ABSTRACT: Pluggable (lite) authentication and authorization server.
 # VERSION
+
+sub startup
+{
+  my($self, $config) = @_;
+
+  $self->plugin('plug_auth_lite',
+    auth => $config->{auth} // sub { 0 },
+    authz => $config->{authz} // sub { 0 },
+  );
+}
 
 =head1 SYNOPSIS
 
@@ -13,24 +24,74 @@ command line:
 
  % plugauthlite
 
-Mojolicious (lite):
+Mojolicious Plugin:
 
  use Mojolicious::Lite;
  
- plugin 'plug_auth_lite';
+ plugin 'plug_auth_lite', 
+   auth => sub {
+     my($user, $pass) = @_;
+     if($user eq 'optimus' && $pass eq 'matrix')
+     { return 1; }
+     else
+     { return 0; }
+   },
+   authz => sub {
+     my($user, $action, $resource) = @_;
+     if($user eq 'optimus && $action eq 'open' && $resource =~ m{^/matrix})
+     { return 1 }
+     else
+     { return 0 }
+   };
+
+Mojolicious App:
+
+ use PlugAuth::Lite;
+ my $app = PlugAuth::Lite->new({
+   auth => sub {
+     my($user, $pass) = @_;
+     if($user eq 'optimus' && $pass eq 'matrix')
+     { return 1; }
+     else
+     { return 0; }
+   },
+   authz => sub {
+     my($user, $action, $resource) = @_;
+     if($user eq 'optimus && $action eq 'open' && $resource =~ m{^/matrix})
+     { return 1 }
+     else
+     { return 0 }
+   },
+ });
 
 =head1 DESCRIPTION
 
-This distribution provides a L<PlugAuth> compatible server in the form of
-a Mojolicious lite application (L<plugauthlite>) and Mojolicious Plugin
-(L<Mojolicious::Plugin::PlugAuthLite>).  In the future it will also contain
-a testing interface for testing authentication and authorization rules
-in L<Clustericious> applications.
+This distribution provides 
+
+=over 4
+
+=item L<plugauthlite>
+
+L<PlugAuth> compatible server in the form of a L<Mojolicious::Lite> application.
+
+=item L<Mojolicious::Plugin::PlugAuthLite>
+
+L<Mojolicious> plugin that adds L<PlugAuth> compatible routes to a new or 
+existing L<Mojolicious> application.
+
+=item L<PlugAuth::Lite>
+
+L<Mojolicious> application with L<PlugAuth> compatible routes that can be spawned
+from within a perl application.
+
+=back
+
+In the future it will also contain a testing interface for testing authentication
+and authorization rules in L<Clustericious> applications.
 
 It has fewer prerequisites that the full fledged L<PlugAuth> server (simply
 L<Mojolicious> and perl itself) but also fewer features (it notably lacks
-the management interface).  See the other modules in this distribution for
-details.
+the management interface).
 
 =head1 SEE ALSO
 
