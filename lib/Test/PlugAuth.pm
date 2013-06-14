14958ecbd8c8abb8eda89a71f7e176b8c97bbe1a
Graham Ollis <perl@wdlabs.com>
2013-06-14 12:31:16 -0400

    doco

--- lib/Test/PlugAuth.pm
+++ lib/Test/PlugAuth.pm
@@ -9,33 +9,6 @@ use Mojo::UserAgent;
 # ABSTRACT: minimum PlugAuth server to test Clustericious apps against
 # VERSION
 
-sub new
-{
-  my $class = shift;
-  my $config = ref $_[0] ? $_[0] : {@_};
-  my $self = bless {}, $class;
-  
-  $self->{app} = PlugAuth::Lite->new($config);
-  $self->{ua}  = Mojo::UserAgent->new;
-  $self->ua->app($self->app);
-  
-  $self->{url} = $self->ua->app_url->to_string;
-  $self->{url} =~ s{/$}{};
-  
-  return $self;
-}
-
-sub ua  { shift->{ua}  }
-sub app { shift->{app} }
-sub url { shift->{url} }
-
-sub apply_to_client_app 
-{
-  my($self, $client_app) = @_;
-  $client_app->helper(auth_ua => sub { $self->ua });
-  return;
-}
-
 =head1 SYNOPSIS
 
 assuming you have a Clustericious app MyApp with authentication/authorization
@@ -67,6 +40,73 @@ directives that you need to test:
 Provides a way to test a Clustericious application with a fake PlugAuth server
 with reduced boilerplate
 
+=head1 CONSTRUCTOR
+
+=head2 Test::PlugAuth->new( $config )
+
+Creates a new instance of Test::PlugAuth.  The $config is passed
+directly into L<PlugAuth::Lite>.  See L<Mojolicious::Plugin::PlugAuthLite>
+for details.
+
 =cut
+
+sub new
+{
+  my $class = shift;
+  my $config = ref $_[0] ? $_[0] : {@_};
+  my $self = bless {}, $class;
+  
+  $self->{app} = PlugAuth::Lite->new($config);
+  $self->{ua}  = Mojo::UserAgent->new;
+  $self->ua->app($self->app);
+  
+  $self->{url} = $self->ua->app_url->to_string;
+  $self->{url} =~ s{/$}{};
+  
+  return $self;
+}
+
+=head1 ATTRIBUTES
+
+=head2 ua
+
+The L<Mojo::UserAgent> used to connect to the PlugAuth (lite) server.
+
+=cut
+
+sub ua  { shift->{ua}  }
+
+=head2 app
+
+The L<PlugAuth::Lite> instance of the PlugAuth server.
+
+=cut
+
+sub app { shift->{app} }
+
+=head2 url
+
+The (fake) url used to connect to the PlugAuth server with.  You MUST
+connect to through the L<Mojo::UserAgent> above.
+
+=cut
+
+sub url { shift->{url} }
+
+=head1 METHODS
+
+=head2 $test_auth->apply_to_client_app( $client_app )
+
+Given a Clustericious application C<$client_app>, this method will 
+rewire our L<Mojo::UserAgent> for authentication requests to PlugAuth.
+
+=cut
+
+sub apply_to_client_app 
+{
+  my($self, $client_app) = @_;
+  $client_app->helper(auth_ua => sub { $self->ua });
+  return;
+}
 
 1;
