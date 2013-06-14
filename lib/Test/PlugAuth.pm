3b3ae7e836ff2ced77c4a25dfed61679911a3253
Graham Ollis <perl@wdlabs.com>
2013-06-14 12:23:46 -0400

    add an actual interface to Test::PlugAuth

--- lib/Test/PlugAuth.pm
+++ lib/Test/PlugAuth.pm
@@ -3,61 +3,69 @@ package Test::PlugAuth;
 use strict;
 use warnings;
 use v5.10;
+use PlugAuth::Lite;
+use Mojo::UserAgent;
 
 # ABSTRACT: minimum PlugAuth server to test Clustericious apps against
 # VERSION
+
+sub new
+{
+  my $class = shift;
+  my $config = ref $_[0] ? $_[0] : {@_};
+  my $self = bless {}, $class;
+  
+  $self->{app} = PlugAuth::Lite->new($config);
+  $self->{ua}  = Mojo::UserAgent->new;
+  $self->ua->app($self->app);
+  
+  $self->{url} = $self->ua->app_url->to_string;
+  $self->{url} =~ s{/$}{};
+  
+  return $self;
+}
+
+sub ua  { shift->{ua}  }
+sub app { shift->{app} }
+sub url { shift->{url} }
+
+sub apply_to_client_app 
+{
+  my($self, $client_app) = @_;
+  $client_app->helper(auth_ua => sub { $self->ua });
+  return;
+}
 
 =head1 SYNOPSIS
 
 assuming you have a Clustericious app MyApp with authentication/authorization
 directives that you need to test:
 
- use File::HomeDir::Test;
- use File::HomeDir;
- use MyApp;
- use Mojo::UserAgent;
- use PlugAuth::Lite;
+ use Test::Clustericious::Config;
+ use Test::Clustericious;
+ use Test::PlugAuth;
  
- my $auth_ua = Mojo::UserAgent->new;
- $auth_ua->app(
-   PlugAuth::Lite->new({
-     # see Mojolicious::Plugin::PlugAuthLite for details
-     auth  => sub {
-       my($user,$pass) = @_;
-       return 1 if $user eq 'gooduser' && $pass eq 'goodpass';
-       return;
-     }
-     authz => sub { 1 }, # return true for authorized
-   })
- );
- 
- # thanks to File::HomeDir::Test, ~/etc is actually /tmp/something/etc
- # and will be removed after the test is complete
- my $home = File::HomeDir->my_home;
- my $auth_url = "http://localhost:" . $auth_ua->app_url->port;
- mkdir "$home/etc";
- DumpFile("$home/etc/SomeService.conf", {
-   plug_auth => {
-     url => $auth_url,
-   },
+ my $auth = Test::PlugAuth->new(auth => {
+   my($user,$pass) = @_;
+   return $user eq 'gooduser' && $pass eq 'goodpass';
  });
  
- my $t = Test::Mojo->new("MyApp");
+ create_config_ok 'MyApp', { plug_auth => $auth->url };
  
- $t->get_ok('/private')
-   ->status_is(401);
+ $t = Test::Clustericious->new('MyApp');
+ $auth->apply_to_client_app($t->app);
  
  my $port = $t->ua->app_url->port;
  
  $t->get_ok("http://baduser:badpass\@localhost:$port/private")
    ->status_is(401);
- $t->geT_ok("http://gooduser:goodpass\@localhost:$port/private")
+ $t->get_ok("http://gooduser:goodpass\@localhost:$port/private")
    ->status_is(200);
 
 =head1 DESCRIPTION
 
-This is a documentation only module.  In the future I will replace this
-module with one that reduces some of the boiler plate above.
+Provides a way to test a Clustericious application with a fake PlugAuth server
+with reduced boilerplate
 
 =cut
 
